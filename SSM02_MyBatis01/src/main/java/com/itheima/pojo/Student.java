5b837af6beb24970548fb7c18ac40f812b8a799a
1234 <2368144523@qq.com>
2026-03-15 13:03:14 +0800

    第二次实验

---
+++ SSM02_MyBatis01/src/main/java/com/itheima/pojo/Student.java
@@ -0,0 +1,40 @@
+package com.itheima.pojo;
+
+public class Student {
+    private Integer id;            //主键id
+    private String name;          //学生姓名
+    private Integer age;          //学生年龄
+
+    public Student() {
+    }
+
+    public Student(Integer id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
